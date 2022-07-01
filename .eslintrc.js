af981385067eec454b4e490b683b52d01ce0968b
lvpv <lv941226@gmail.com>
2022-07-01 16:26:51 +0800

    init project

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true
+  },
+  extends: ['plugin:vue/vue3-recommended', 'standard', 'plugin:prettier/recommended'],
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
+    sourceType: 'module'
+  },
+  plugins: ['vue', '@typescript-eslint', 'prettier'],
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'prettier/prettier': 'error',
+    'vue/multi-word-component-names': 'off'
+  }
+}
---
+++ src/components/header/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Hello</h1>
+  <h2>{{ count }}</h2>
+  <button @click="increment">+1</button>
+</template>
+
+<script lang="ts" setup name="Header">
+  import { ref } from 'vue'
+  const count = ref(0)
+  const increment = () => count.value++
+</script>
+
+<style scoped></style>
